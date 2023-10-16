bdc51960e6da809920ea63ef46154bf99370587d
ArunLohmod <79635124+ArunLohmod@users.noreply.github.com>
2023-10-16 16:59:00 +0530

    minor changes

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -10,7 +10,10 @@ contract MyToken is ERC721, Ownable {
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("MyToken", "MTK") {}
+    constructor()
+        ERC721("MyToken", "MTK")
+        Ownable(msg.sender)
+    {}
 
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
